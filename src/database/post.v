f3e26d3b99bc12d2a2736159096aa83cfff5254b
EmmaTheMartian <emmathemartian@gmail.com>
2025-01-08 18:14:29 -0600

    post searching implemented

--- src/database/post.v
+++ src/database/post.v
@@ -163,11 +163,20 @@ pub fn PostSearchResult.from_post_list(app &DatabaseAccess, posts []Post) []Post
 // search_for_post searches for posts matching the given query.
 // todo: query options/filters, such as user:beep, !excluded-text, etc
 pub fn (app &DatabaseAccess) search_for_posts(query string, limit int, offset int) []PostSearchResult {
-	println('searching, q=${query},l=${limit},o=${offset}')
-	//todo: app.db.q_strings('select from Post where title like \'%${query}%\' or body like \'%${query}%\' order by posted_at desc limit limit offset offset')
-	posts := sql app.db {
-		select from Post where title like '%${query}%' order by posted_at desc limit limit offset offset
-	} or { [] }
-	println('search results: ${posts.len}')
+	sql_query := "\
+	SELECT *, CASE
+		WHEN title LIKE '%${query}%' THEN 1
+		WHEN body LIKE '%${query}%' THEN 2
+		END AS priority
+	FROM \"Post\"
+	WHERE title LIKE '%${query}%' OR body LIKE '%${query}%'
+	ORDER BY priority ASC LIMIT ${limit} OFFSET ${offset}"
+
+	queried_posts := app.db.q_strings(sql_query) or {
+		eprintln('search_for_posts error in app.db.q_strings: ${err}')
+		[]
+	}
+
+	posts := queried_posts.map(|it| Post.from_row(it))
 	return PostSearchResult.from_post_list(app, posts)
 }
--- src/entity/post.v
+++ src/entity/post.v
@@ -1,6 +1,8 @@
 module entity
 
+import db.pg
 import time
+import util
 
 pub struct Post {
 pub mut:
@@ -14,4 +16,25 @@ pub mut:
 	pinned bool
 
 	posted_at time.Time = time.now()
+}
+
+// Post.from_row creates a post from the given database row.
+// see src/database/post.v#search_for_posts for usage.
+@[inline]
+pub fn Post.from_row(row pg.Row) Post {
+	// this throws a cgen error when put in Post{}
+	//todo: report this
+	posted_at := time.parse(util.or_throw[string](row.vals[6])) or { panic(err) }
+
+	return Post{
+		id: util.or_throw[string](row.vals[0]).int()
+		author_id: util.or_throw[string](row.vals[1]).int()
+		replying_to: if row.vals[2] == none { ?int(none) } else {
+			util.map_or_throw[string, int](row.vals[2], |it| it.int())
+		}
+		title: util.or_throw[string](row.vals[3])
+		body: util.or_throw[string](row.vals[4])
+		pinned: util.map_or_throw[string, bool](row.vals[5], |it| it.bool())
+		posted_at: posted_at
+	}
 }
