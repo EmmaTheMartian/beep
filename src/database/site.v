49a74f3b9d8e54caddec42531411196556f60021
EmmaTheMartian <emmathemartian@gmail.com>
2025-11-29 13:12:15 -0600

    reload configs in get_or_create_site_config() after creating a new config

--- src/database/site.v
+++ src/database/site.v
@@ -3,7 +3,7 @@ module database
 import entity { Site }
 
 pub fn (app &DatabaseAccess) get_or_create_site_config() Site {
-	configs := sql app.db {
+	mut configs := sql app.db {
 		select from Site
 	} or { [] }
 	if configs.len == 0 {
@@ -12,6 +12,9 @@ pub fn (app &DatabaseAccess) get_or_create_site_config() Site {
 		sql app.db {
 			insert site_config into Site
 		} or { panic('failed to create site config (${err})') }
+		configs = sql app.db {
+			select from Site
+		} or { [] }
 	} else if configs.len > 1 {
 		// this should never happen
 		panic('there are multiple site configs')
